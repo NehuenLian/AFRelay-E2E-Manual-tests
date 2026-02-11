4214c758f8839365e6d69d177e6e18c9c6b68cb4
Nehuen Lian <nehuenn.lian@gmail.com>
2026-02-10 22:33:44 -0300

    add e2e scripts for remaining endpoints

---
+++ wsfe/fe_param_get_tipos_doc.py
@@ -0,0 +1,28 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+DEPLOYMENT_URL = os.getenv("DEPLOYMENT_URL")
+CUIT = os.getenv("CUIT")
+TOKEN = os.getenv("TOKEN")
+
+def fe_param_get_tipos_doc():
+
+    url = f"{DEPLOYMENT_URL}/wsfe/FEParamGetTiposDoc"
+
+    data = {
+        "Auth": {
+            "Cuit": CUIT
+        }
+    }
+
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+
+    response = requests.post(url, json=data, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+response = fe_param_get_tipos_doc()
+print(response)
